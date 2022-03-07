1bb23ac84823ca075d1abdcaac631e5d5d22d785
Jiklopo <43034313+Jiklopo@users.noreply.github.com>
2022-03-07 21:11:30 +0600

    added password validation

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 from rest_framework import status
 from rest_framework import mixins
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
-from drf_spectacular.utils import extend_schema
+from drf_spectacular.utils import extend_schema, OpenApiResponse, OpenApiExample
+from drf_spectacular.types import OpenApiTypes
 
 from apps.authentication.models import User
 from apps.authentication.serializers import PasswordSerializer, UserInputSerializer
@@ -16,9 +19,25 @@ class RegisterView(GenericAPIView, mixins.CreateModelMixin):
 
     @extend_schema(
         request=UserInputSerializer,
-        responses={201: '', 400: ''}
+        responses={
+            201: None,
+            400: OpenApiResponse(
+                [OpenApiTypes.STR],
+                description='errors dictionary'
+            )
+        }
     )
     def post(self, request, *args, **kwargs):
+        password = request.data.get('password')
+        if not password:
+            return self.create(request, *args, **kwargs)
+
+        try:
+            validate_password(password)
+        except ValidationError as e:
+            data = {msg.code: msg.messages[0] for msg in e.error_list}
+            return Response(data, status.HTTP_400_BAD_REQUEST)
+
         return self.create(request, *args, **kwargs)
 
 
@@ -38,7 +57,8 @@ class DeactivateAccountView(IsAuthenticatedView):
 
         user: User = request.user
         if not user.check_password(password):
-            return Response('Incorrect password provided', status.HTTP_403_FORBIDDEN)
+            data = {'error': 'Incorrect password provided'}
+            return Response(data, status.HTTP_403_FORBIDDEN)
 
         user.is_active = False
         user.save()
