e84a424e290f6fc859016c83c1624a90754e040b
Jiklopo <43034313+Jiklopo@users.noreply.github.com>
2022-03-22 20:04:58 +0600

    fixed user phone field

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -7,6 +7,6 @@ from apps.common.models import TimeStampModel
 
 class User(AbstractUser, TimeStampModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    phone_number = models.CharField(max_length=16, default='')
+    phone_number = models.CharField(max_length=32, null=True, blank=True)
     is_premium = models.BooleanField(default=False)
     is_doctor = models.BooleanField(default=False)
