1bf376c409f7660a65d560ba825e1ca397ceb155
Jiklopo <43034313+Jiklopo@users.noreply.github.com>
2022-06-04 19:55:58 +0600

    fixed health views schema

--- apps/health/views.py
+++ apps/health/views.py
@@ -1,3 +1,4 @@
+from urllib import response
 from drf_spectacular.utils import extend_schema
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -7,19 +8,28 @@ from apps.health.tasks import test_task
 
 
 class HealthView(APIView):
-    @extend_schema(description='Check connection to the server')
+    @extend_schema(
+        description='Check connection to the server',
+        responses={200:None}
+    )
     def get(self, request):
         return Response()
 
 
 class AuthHealthView(IsAuthenticatedView):
-    @extend_schema(description='Check authorization')
+    @extend_schema(
+        description='Check authorization', 
+        responses={200:None}
+    )
     def get(self, request):
         return Response()
 
 
 class CeleryHealthView(IsAuthenticatedView):
-    @extend_schema(description='Run test celery task')
+    @extend_schema(
+        description='Run test celery task',
+        responses={200:None}
+        )
     def post(self, request):
         task = test_task.delay()
-        return Response(task.id)
+        return Response({'task_id':task.id})
